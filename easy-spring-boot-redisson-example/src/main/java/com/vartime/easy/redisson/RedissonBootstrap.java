4865e57b6090429fabc585f02e07f0eafb48edc9
殷林 <yinlin_cd@keruyun.com>
2019-03-14 11:19:02 +0800

    【U】新增redisson example

---
+++ easy-spring-boot-redisson-example/src/main/java/com/vartime/easy/redisson/RedissonBootstrap.java
@@ -0,0 +1,21 @@
+package com.vartime.easy.redisson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : yinlin
+ * @version : 1.0
+ * @date : 2019-03-14 11:09
+ * @Description :
+ * @Copyright : Copyright (c) 2018
+ * @Company : KeRuYun Technology(Beijing) Chengdu Co. Ltd.
+ * @link : com.vartime.easy.redisson.RedissonBootstrap
+ */
+@SpringBootApplication
+public class RedissonBootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonBootstrap.class, args);
+    }
+}
